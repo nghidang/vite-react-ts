f344cf7d47c9f7ef7a08079ba5038454a7046f81
Nghi Dang <dangquocnghi@gmail.com>
2026-06-16 10:14:08 +0700

    Layout đang nằm ở hai nơi:
    src/layouts/ → MainLayout, MainLayoutAuth, layouts.css
    src/components/layout/ → Header/, Footer/
    → Header/Footer là bộ phận cấu thành của layout nhưng lại ở nhánh khác. Đề xuất gom về một chỗ:
    
    src/layouts/
      MainLayout.tsx
      MainLayoutAuth.tsx
      Header/
      Footer/
      layouts.css
    Header/Footer không được tái dùng ngoài layout nên không cần ở components/common.

--- src/layouts/MainLayoutAuth.tsx
+++ src/layouts/MainLayoutAuth.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import { RouteFallback } from '../components/common/RouteFallback/RouteFallback'
-import { Footer } from '../components/layout/Footer/Footer'
-import { Header } from '../components/layout/Header/Header'
+import { Footer } from './Footer/Footer'
+import { Header } from './Header/Header'
 import './layouts.css'
 
 export function MainLayoutAuth() {
